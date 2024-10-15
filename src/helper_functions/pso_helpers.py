09494015d25c7a1c506e737b57b9c9fae5a376e1
CamyH <cameronhunt88@hotmail.com>
2024-10-15 13:51:02 +0100

    Add rough velocity update implementation

--- src/helper_functions/pso_helpers.py
+++ src/helper_functions/pso_helpers.py
@@ -1,22 +1,33 @@
+import random
+import numpy as np
+
+from src.helper_functions.helpers import mse
+
+
 # Calculating the new velocity
 def update_velocity(current_velocity,
-                    inertia,
                     cognitive_weight,
                     social_component,
                     particle_current_pos,
                     particle_best_pos,
-                    particle_informant_best_pos):
-    # Calculating the new velocity
-    new_velocity = (inertia * current_velocity +
-                    cognitive_weight(particle_best_pos - particle_current_pos) +
-                    social_component(particle_informant_best_pos - particle_current_pos))
+                    particle_informant_best_pos,
+                    inertia=1,
+                    beta=1,
+                    gamma=1,
+                    delta=1):
 
-    return new_velocity
+    b = random.uniform(0.0, beta)
+    c = random.uniform(0.0, gamma)
+    d = random.uniform(0.0, delta)
+
+    new_velocity = (inertia * current_velocity +
+                       b * (particle_best_pos - particle_current_pos) +
+                       c * (particle_informant_best_pos - particle_current_pos))
+
+    return new_velocity[0]
 
 # Mean Squared Error has been chosen
 # for the fitness function
-def calculate_fitness(particle_best_pos,
-                      particle_current_pos):
-    # train the ANN here and then we evaluate the fitness using MSE
-    return 'not implemented'
+def calculate_fitness(particle):
+    return mse(particle, particle)
 
