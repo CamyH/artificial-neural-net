d03d2dda9454a9c3f4be4575d44d5566d1ae3d6f
CamyH <cameronhunt88@hotmail.com>
2024-10-15 13:51:14 +0100

    Add method call for PSO algorithm

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from src.activation_functions.activation_functions import relu
+from src.pso.pso import particle_swarm_optimisation
 
 if __name__ == '__main__':
-    print('not implemented yet')
+    particle_swarm_optimisation(5)
