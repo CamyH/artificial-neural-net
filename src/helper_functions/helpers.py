53eb4ff84c5192e693ef5368f6cfd1430d216fe9
CamyH <cameronhunt88@hotmail.com>
2024-10-16 14:16:15 +0100

    Add train test split function

--- src/helper_functions/helpers.py
+++ src/helper_functions/helpers.py
@@ -10,3 +10,11 @@ def mse(n, y, yhat):
         raise Exception('data points cannot be zero for MSE')
 
     return 1 / n * np.sum((yhat - y) ** 2)
+
+# Split the data into train and test data
+def train_test_split(arr, percent=0.2):
+    split_idx = int(len(arr) * percent)
+    arr_train = arr[:split_idx]
+    arr_test = arr[split_idx:]
+
+    return arr_train, arr_test
