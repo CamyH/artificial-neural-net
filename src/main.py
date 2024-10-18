2968a0c5b19cf7bff498c0c84e8e75b4f864b10e
CamyH <cameronhunt88@hotmail.com>
2024-10-18 20:37:08 +0100

    Add temp data import for testing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from src.activation_functions.activation_functions import relu
 from src.pso.pso import particle_swarm_optimisation
+import pandas as pd
 
 if __name__ == '__main__':
-    particle_swarm_optimisation(5)
+    data = pd.read_csv('./data/concrete_data.csv')
+
+    particle_swarm_optimisation(data.shape[0], data['concrete_compressive_strength'].values , 5, 7)
