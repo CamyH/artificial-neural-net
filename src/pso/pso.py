566bc416a73b8e03d7794fde4c873c5a387472e3
CamyH <cameronhunt88@hotmail.com>
2024-10-18 20:36:57 +0100

    Refactor to pass in correct params

--- src/pso/pso.py
+++ src/pso/pso.py
@@ -4,42 +4,37 @@ import random
 from src.helper_functions.pso_helpers import *
 
 
-def particle_swarm_optimisation(swarm_size):
+def particle_swarm_optimisation(data_points,
+                                target_values,
+                                swarm_size,
+                                dimensions):
     iterations = 0
 
-    particles = np.empty((0, swarm_size))
+    particles = np.random.rand(swarm_size, dimensions)
 
     cognitive_weight = 1.2
     social_component = 1.4
 
     for i in range(swarm_size):
-        particles = np.vstack([particles, np.hstack([np.random.rand(swarm_size)])])
-
-    #print(particles)
+        particles = np.vstack([particles, np.hstack([np.random.rand(swarm_size, dimensions)])])
 
     personal_bests = np.copy(particles)
 
-    velocities = np.random.uniform(-1, 1, size=swarm_size)
-    velocity = 0
-
-    #print('velocities ', velocities[1])
+    velocities = np.random.uniform(-1, 1, (swarm_size, dimensions))
 
     # Initialised to the first personal best
     best = np.full(swarm_size, -np.inf)
-    #print('best', best)
-
-    idx = 0
 
     while iterations < 10:
         for idx, particle in enumerate(particles):
-            fitness = calculate_fitness(particle)
+            fitness = calculate_fitness(data_points, target_values, true_target_values)
             print(fitness)
 
-            if best[idx] == 0 or fitness > best[idx]:
+            if best[idx] == -np.inf or fitness > best[idx]:  # Change from 0 to -np.inf for initialization
                 best[idx] = fitness
                 personal_bests[idx] = particle.copy()
 
-            for dimension in particle:
+            for dimension in range(dimensions):
                 beta = random.uniform(0.0, 5.0)
                 gamma = random.uniform(0.0, 10)
                 delta = random.uniform(0.0, 15)
@@ -51,6 +46,7 @@ def particle_swarm_optimisation(swarm_size):
                                                   best[idx],
                                                   personal_bests[idx])
 
+                particles[idx][dimension] += velocities[idx][dimension]
                 #print(velocities[idx])
                 #print('velocities at idx ', velocities[idx])
                 #velocities[idx] = velocity
