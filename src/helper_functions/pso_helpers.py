dc62f5190915cf1ae7317b071bec6242988dade3
CamyH <cameronhunt88@hotmail.com>
2024-10-21 14:33:42 +0100

    Expand comment for update_velocity

--- src/helper_functions/pso_helpers.py
+++ src/helper_functions/pso_helpers.py
@@ -7,6 +7,7 @@ from src.helper_functions.helpers import mse
 # Calculating the new velocity
 # Used: https://stackoverflow.com/questions/15069998/particle-swarm-optimization-inertia-factor
 # For guidance on the inertia constant
+# Set to 0.8 for now, may investigate decreasing the inertia over time in future
 def update_velocity(current_velocity,
                     cognitive_weight,
                     social_component,
