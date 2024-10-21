7c1c65db5a383a2cdda3b3ca7004021962d098f9
CamyH <cameronhunt88@hotmail.com>
2024-10-21 15:14:18 +0100

    Refactor mse implementation to be more readable

--- src/helper_functions/helpers.py
+++ src/helper_functions/helpers.py
@@ -4,12 +4,9 @@ import numpy as np
 
 # n = data points
 # y = observed values
-# yhat = predicted values
-def mse(n, y, yhat):
-    if n == 0:
-        raise Exception('data points cannot be zero for MSE')
-
-    return 1 / n * np.sum((yhat - y) ** 2)
+# pred = predicted values/yhat
+def mse(n, y, pred):
+    return 1 / n * np.sum((pred - y) ** 2)
 
 # Split the data into train and test data
 def train_test_split(arr, percent=0.2):
