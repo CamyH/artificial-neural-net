56205853cb4ea95e261f4c457a674cdfa4be5b69
CamyH <cameronhunt88@hotmail.com>
2024-10-28 10:41:16 +0000

    Fix MSE

--- src/helper_functions/helpers.py
+++ src/helper_functions/helpers.py
@@ -1,12 +1,8 @@
 # Mean Squared Error (MSE) chosen for regression
-
-import numpy as np
-
-# n = data points
 # y = observed values
 # pred = predicted values/yhat
-def mse(n, y, pred):
-    return 1 / n * np.sum((pred - y) ** 2)
+def mse(y, pred):
+    return (pred - y) ** 2
 
 # Split the data into train and test data
 def train_test_split(arr, percent=0.2):
