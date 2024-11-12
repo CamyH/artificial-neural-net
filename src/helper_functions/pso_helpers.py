c01783a97a33bac707b72c99b1dc63aea47c84b7
CamyH <cameronhunt88@hotmail.com>
2024-11-12 12:26:21 +0000

    Add forward pass for PSO

--- src/helper_functions/pso_helpers.py
+++ src/helper_functions/pso_helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from src.activation_functions.activation_functions import relu
 from src.helper_functions.helpers import mse
 import random
 
@@ -17,8 +18,8 @@ def update_velocity(current_velocity,
                     best,
                     inertia=0.7):
 
-    b = random.uniform(0.0, cognitive_weight)
-    c = random.uniform(0.0, social_component)
+    b = cognitive_weight
+    c = social_component
 
     # Update the velocity using the PSO formula
     new_velocity = (inertia * current_velocity +
@@ -28,14 +29,26 @@ def update_velocity(current_velocity,
     # We want to use Numpy.Clip to ensure that none of the values
     # take us out of bounds (are too big)
     # https://numpy.org/doc/stable/reference/generated/numpy.clip.html
-    new_velocity = np.clip(new_velocity, -0.6, 0.6)
+    new_velocity = np.clip(new_velocity, -0.7, 0.7)
 
     return new_velocity
 
 # Mean Squared Error has been chosen
 # for the fitness function
-def calculate_fitness(y_pred, y_train):
+def calculate_fitness(y_pred, y_train, layers, particle):
+    y_pred = forward_pass_pso(y_pred, layers, particle)
     return mse(y_train, y_pred)
+
+# Forward Pass
+# Takes in Input Data, Weights and Biases
+def forward_pass_pso(data, layers, particle):
+    output = data
+    for i in range(layers):
+        weights = particle['weights'][i]
+        bias = particle['biases'][i]
+        ws = np.dot(output, weights) + bias
+        output = relu(ws)
+    return np.dot(output, particle['weights_output']) + particle['biases'][-1][layers]
 
 # Randomly assign n number of informants
 # Default is 2, can be overridden
