2af7e270cdf3114e708ddcbec04c0c7e2d30e1f4
CamyH <cameronhunt88@hotmail.com>
2024-11-12 10:22:07 +0000

    Update train test split

--- src/helper_functions/helpers.py
+++ src/helper_functions/helpers.py
@@ -6,9 +6,13 @@ import numpy as np
 def mse(y, pred):
     return np.mean((pred - y) ** 2)
 
+# Mean Absolute Error
+def mae(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
 # Split the data into train and test data
 def train_test_split(arr, percent=0.2):
-    split_idx = int(len(arr) * percent)
+    split_idx = int(len(arr) * (1 - percent))
     arr_train = arr[:split_idx]
     arr_test = arr[split_idx:]
 
