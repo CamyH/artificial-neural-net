783337855ba9aaab3281f3ce99000a3a4c4bb4a9
CamyH <cameronhunt88@hotmail.com>
2024-11-14 18:46:18 +0000

    Remove particle initialisation from neural network

--- src/ann/neural_net.py
+++ src/ann/neural_net.py
@@ -6,8 +6,7 @@ import time
 
 # Custom Helpers
 from src.activation_functions.activation_functions import relu
-from src.helper_functions.helpers import train_test_split, mse, mae
-from src.helper_functions.pso_helpers import init_informants
+from src.helper_functions.helpers import train_test_split, mse, mae, init_weights_biases
 from src.pso.pso import particle_swarm_optimisation
 
 
@@ -26,48 +25,18 @@ def predict(layers, nodes, swarm_size=3):
 
     # Param Initialisation
     particles = []
-    max_weight = 0.3
-    min_weight = -0.3
-    for j in range(swarm_size):
-        particle = {
-            'weights': [],
-            'weights_output': np.random.uniform(min_weight, max_weight, (nodes, 1)),
-            'biases': [],
-            'velocities': [],
-            'bias_velocities': np.random.uniform(-0.1, 0.1, dimensions),
-            'fitness': 9999,
-            'personal_best': [],
-            'personal_best_fitness': 9999,
-            'informants': []
-        }
 
-        prev_layer_nodes = dimensions
-        for i in range(layers):
-            weights = np.random.uniform(min_weight, max_weight, (prev_layer_nodes, nodes))
-            biases = np.full(nodes, 0.1)
-            velocities = np.random.uniform(-0.3, 0.3, (prev_layer_nodes, nodes))
-            personal_best = np.zeros((prev_layer_nodes, nodes))
+    weights = []
+    biases = []
 
-            #print(f'Layer {i}, weights shape {weights.shape} and velocities shape {velocities.shape}')
-
-            particle['weights'].append(weights)
-            particle['biases'].append(biases)
-            particle['velocities'].append(velocities)
-            particle['personal_best'].append(personal_best)
-
-            prev_layer_nodes = nodes
-
-        particles.append(particle)
-
-    # Initialise Informants
-    particles = init_informants(particles)
+    weights, biases = init_weights_biases(layers, weights, biases)
 
     # Train, Test Split
     x_train, x_test = train_test_split(x)
     y_train, y_test = train_test_split(y)
 
     # Misc Params
-    num_iterations = 10000
+    num_iterations = 1000
     start_time = time.time()
     np.random.seed(1)
 
@@ -83,18 +52,19 @@ def predict(layers, nodes, swarm_size=3):
                                   particle)
 
             # PSO
-            pso_output = particle_swarm_optimisation(forward_pass_output,
+            pso_output = particle_swarm_optimisation(swarm_size,
+                                                    forward_pass_output,
                                                     output,
                                                     dimensions,
-                                                    particles)
-            #print('weights ', particle['weights'])
-            #print('pso_output ', pso_output['weights'])
+                                                    particles,
+                                                     layers,
+                                                     nodes)
+
             particle['weights'] = pso_output['weights']
             # For testing
             #df = pd.DataFrame(output)
             #df.to_csv('forward_pass_out.csv')
 
-    #print(output.shape)
     result = mse(y_train, output)
     result_mae = mae(y_train, output)
     print('Training time: ', str(time.time() - start_time))
@@ -113,3 +83,4 @@ def forward_pass(data, layers, particle):
         output = relu(ws)
 
     return np.dot(output, particle['weights_output']) + particle['biases'][-1][layers]
+
