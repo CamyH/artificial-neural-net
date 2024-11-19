09d57af56e8e259a3668b3e6444e4151cdaf1bc9
CamyH <cameronhunt88@hotmail.com>
2024-11-19 15:53:35 +0000

    Refactor some of neural net to be closer to spec

--- src/ann/neuralnetwork.py
+++ src/ann/neuralnetwork.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class ANN:
+class NeuralNetwork:
     def __init__(self, layer_sizes, activation_functions):
         """
         Initialize the ANN with specified layers and activation functions.
@@ -15,7 +15,6 @@ class ANN:
         self.biases = [np.random.randn(layer_sizes[i], 1) * 0.1 for i in range(1, len(layer_sizes))]
 
     def activate(self, x, func):
-        """Applies the specified activation function."""
         if func == 'linear':
             return x
         elif func == 'logistic':
@@ -27,16 +26,19 @@ class ANN:
         else:
             raise ValueError("Unknown activation function.")
 
-    def forward(self, inputs):
-        """Forward pass through the network."""
-        a = inputs
-        for i in range(len(self.weights)):
-            z = np.dot(self.weights[i], a) + self.biases[i]
-            a = self.activate(z, self.activation_functions[i])
-        return a
+    # Forward Pass
+    # Takes in the set of inputs, returns the predicted outputs
+    def forward_pass(self, data):
+        output = data
+        for i in range(len(self.weights) - 1):
+            ws = np.dot(self.weights[i], output) + self.biases[i]
+            output = self.activate(ws, self.activation_functions[i])
 
-    def set_parameters(self, params):
-        """Set weights and biases from a flat parameter vector."""
+        # Don't want to apply any activation function on the output
+        return np.dot(self.weights[-1], output) + self.biases[-1]
+
+    # Update the params of the network with new ones
+    def update_parameters(self, params):
         idx = 0
         for i in range(len(self.weights)):
             weight_shape = self.weights[i].shape
