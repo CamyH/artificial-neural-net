9f3e5f414a033044a9bb740a489a9c9e862f67a9
CamyH <cameronhunt88@hotmail.com>
2024-11-21 22:38:13 +0000

    Add updated boundary handling

--- src/helper_functions/pso_helpers.py
+++ src/helper_functions/pso_helpers.py
@@ -19,19 +19,20 @@ def update_particle(current_velocity,
                     c3,
                     best_informant,
                     jump_size=0.5,
-                    inertia=0.5):
+                    inertia=0.5,
+                    max_velocity=0.1):
     r1, r2 = np.random.rand(), np.random.rand()
     cognitive_component = c1 * r1 * (particle_best_position - particle_current_position)
     social_component = c2 * r2 * (global_best_position - particle_current_position)
-    informant_component = c3 * (best_informant - particle_current_position)
-
+    informant_component = (c3 + jump_size) * (best_informant - particle_current_position)
     new_velocity = inertia * current_velocity + cognitive_component + social_component + informant_component
 
     # Add step to velocity to help exploration
     new_velocity = new_velocity + jump_size * (best_informant - particle_current_position)
 
     # Boundary handling
-    new_velocity = np.clip(new_velocity, -0.6, 0.6)
+    # If the velocity exceeds 0.1 then we want to limit it
+    new_velocity = new_velocity * min(1, max_velocity / np.linalg.norm(new_velocity))
 
     updated_position = particle_current_position + new_velocity
 
@@ -42,14 +43,15 @@ def update_particle(current_velocity,
 def calculate_fitness(layer_dimensions,
                       data,
                       labels,
-                      particle_position):
+                      particle_position,
+                      activation_function):
     # Initialise the weights and baises for the neural net
     weights, biases = setup(layer_dimensions)
 
     # Forward pass to generate predictions
     predictions = []
     for item in data:
-        predictions.append(predict(item.reshape(-1, 1), weights, biases, particle_position))
+        predictions.append(predict(item.reshape(-1, 1), weights, biases, particle_position, activation_function))
     predictions = np.array(predictions).flatten()
     return mse(labels, predictions)
 
